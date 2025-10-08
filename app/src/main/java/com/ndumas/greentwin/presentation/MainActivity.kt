022e8271f7567e29059c2fd2a41c0fbfb85e640a
hidemet <108723958+hidemet@users.noreply.github.com>
2025-10-08 17:54:29 +0200

    feat: Create core package structure

--- app/src/main/java/com/ndumas/greentwin/presentation/MainActivity.kt
+++ app/src/main/java/com/ndumas/greentwin/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ndumas.greentwin
+package com.ndumas.greentwin.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ndumas.greentwin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
