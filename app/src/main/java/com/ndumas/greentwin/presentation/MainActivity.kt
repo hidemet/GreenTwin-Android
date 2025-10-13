b38cb339df30f613a313865bc1e1c6d07528f4fd
hidemet <108723958+hidemet@users.noreply.github.com>
2025-10-13 13:23:27 +0200

    feat: Configure Gradle with KSP, Hilt and core dependencies

--- app/src/main/java/com/ndumas/greentwin/presentation/MainActivity.kt
+++ app/src/main/java/com/ndumas/greentwin/presentation/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.ndumas.greentwin.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.ndumas.greentwin.R
+import dagger.hilt.android.AndroidEntryPoint
 
+// Questa Activity ospiterà i nostri Fragment
+// che a loro volta useranno Hilt per l'iniezione dei ViewModel.
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // logica di navigazione da implementare
     }
 }
